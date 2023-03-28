8d91a8c0bca485444d2e5cc41a55c996cd2c71b6
Alibek Manabayev <minmax@google.com>
2023-03-28 16:56:55 +0000

    generate: split

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -1,73 +1,98 @@
 package main
 
 import (
-	"errors"
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
+	"os"
+	"path"
+	"sync"
 
 	"github.com/schollz/progressbar/v3"
 
 	"github.com/m-nny/sudoku-solver/sudoku/v2"
 )
 
+var seedPtr = flag.Int64("seed", -1, "seed")
 var NPtr = flag.Int("n", 100, "# of puzzles to generate")
 var hintsPtr = flag.Int("hints", 30, "# of hints")
+var workersPtr = flag.Int("workers", 8, "# of parallel workers")
 var debugPtr = flag.Bool("debug", false, "enable debugging logs")
+var outputDirPtr = flag.String("output", "generated", "output directory")
 
 func main() {
 	flag.Parse()
-	n := *NPtr
-	hints := *hintsPtr
-	puzzles, err := generate(n, hints)
+	if *seedPtr != -1 {
+		rand.Seed(*seedPtr)
+	}
+	puzzles, err := generate(*NPtr, *hintsPtr, *workersPtr)
 	if err != nil {
 		fmt.Printf("Could not generate puzzles: %v\n", err)
 	}
-	if err := solve(puzzles); err != nil {
-		fmt.Printf("Could not solve generated puzzles: %v\n", err)
+	outputFile := path.Join(*outputDirPtr, fmt.Sprintf("n%d-hints%d.csv", *NPtr, *hintsPtr))
+	if err := saveDataset(outputFile, puzzles); err != nil {
+		fmt.Printf("Could not generate puzzles: %v\n", err)
 	}
 }
 
-type Stats struct {
-	noSolution        int
-	mutlipleSolutions int
-	ok                int
+func saveDataset(filepath string, puzzles []string) error {
+	if err := os.MkdirAll(path.Dir(filepath), 0700); err != nil {
+		return err
+	}
+	f, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	csvWriter := csv.NewWriter(f)
+	csvWriter.Write([]string{"puzzle"})
+	puzzlesTable := make([][]string, len(puzzles))
+	for i, puzzle := range puzzles {
+		puzzlesTable[i] = []string{puzzle}
+	}
+	return csvWriter.WriteAll(puzzlesTable)
 }
 
-func generate(n, hints int) ([]string, error) {
+func generate(n, hints, workers int) ([]string, error) {
+	var wg sync.WaitGroup
+	jobs := make(chan int, n)
+	results := make(chan string, n)
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go generateWorker(jobs, results, &wg)
+	}
+	for i := 0; i < n; i++ {
+		jobs <- hints
+	}
+	close(jobs)
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
 	bar := progressbar.Default(int64(n), "generate")
 	defer bar.Finish()
+
 	var puzzles []string
-	for i := 0; i < n; i++ {
-		puzzle, err := sudoku.Generate(hints)
-		if err != nil {
-			return nil, err
-		}
+	for puzzle := range results {
 		puzzles = append(puzzles, puzzle)
 		bar.Add(1)
 	}
 	return puzzles, nil
 }
-func solve(puzzles []string) error {
-	bar := progressbar.Default(int64(len(puzzles)), "solve")
-	defer bar.Finish()
-	var stats Stats
-	for _, puzzle := range puzzles {
-		if *debugPtr {
-			fmt.Printf("solving: %s\n%s\n", puzzle, sudoku.PrettySudoku(puzzle))
+
+func generateWorker(jobs <-chan int, results chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for hints := range jobs {
+		puzzle, err := sudoku.Generate(hints)
+		if err != nil {
+			if *debugPtr {
+				fmt.Printf("error generating puzzle: %v", err)
+			}
+			continue
 		}
-		_, err := sudoku.Solve(puzzle)
-		if errors.Is(err, sudoku.NoSolutionErr) {
-			stats.noSolution++
-			// return err
-		} else if errors.Is(err, sudoku.MultipleSolutionsErr) {
-			stats.noSolution++
-		} else if err != nil {
-			return err
-		} else {
-			stats.ok++
-		}
-		bar.Add(1)
+		results <- puzzle
 	}
-	fmt.Printf("total %d\nno solution %d\nmultiple solutions %d\n ok %d\n", len(puzzles), stats.noSolution, stats.mutlipleSolutions, stats.ok)
-	return nil
 }
