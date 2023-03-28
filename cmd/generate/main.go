2f6cb1f7df50b5e3f3cd5b4cd668cf94d6d81392
Alibek Manabayev <minmax@google.com>
2023-03-28 17:25:05 +0000

    use shared parallel_solver

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -7,18 +7,14 @@ import (
 	"math/rand"
 	"os"
 	"path"
-	"sync"
 
-	"github.com/schollz/progressbar/v3"
-
-	"github.com/m-nny/sudoku-solver/pkg/solver"
+	"github.com/m-nny/sudoku-solver/pkg/parallel_solver"
 )
 
 var seedPtr = flag.Int64("seed", -1, "seed")
 var NPtr = flag.Int("n", 100, "# of puzzles to generate")
 var hintsPtr = flag.Int("hints", 30, "# of hints")
 var workersPtr = flag.Int("workers", 8, "# of parallel workers")
-var debugPtr = flag.Bool("debug", false, "enable debugging logs")
 var outputDirPtr = flag.String("output", "generated", "output directory")
 
 func main() {
@@ -56,43 +52,13 @@ func saveDataset(filepath string, puzzles []string) error {
 }
 
 func generate(n, hints, workers int) ([]string, error) {
-	var wg sync.WaitGroup
-	jobs := make(chan int, n)
-	results := make(chan string, n)
-	for i := 0; i < workers; i++ {
-		wg.Add(1)
-		go generateWorker(jobs, results, &wg)
-	}
-	for i := 0; i < n; i++ {
-		jobs <- hints
-	}
-	close(jobs)
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-
-	bar := progressbar.Default(int64(n), "generate")
-	defer bar.Finish()
-
+	results := parallel_solver.Generate(n, hints, workers)
 	var puzzles []string
-	for puzzle := range results {
-		puzzles = append(puzzles, puzzle)
-		bar.Add(1)
+	for _, puzzle := range results {
+		if puzzle.Err != nil {
+			return puzzles, puzzle.Err
+		}
+		puzzles = append(puzzles, puzzle.Puzzle)
 	}
 	return puzzles, nil
-}
-
-func generateWorker(jobs <-chan int, results chan<- string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for hints := range jobs {
-		puzzle, err := sudoku.Generate(hints)
-		if err != nil {
-			if *debugPtr {
-				fmt.Printf("error generating puzzle: %v", err)
-			}
-			continue
-		}
-		results <- puzzle
-	}
 }
--- pkg/parallel_solver/solve.go
+++ pkg/parallel_solver/solve.go
@@ -1,2 +1,67 @@
 package parallel_solver
 
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	sudoku "github.com/m-nny/sudoku-solver/pkg/solver"
+	"github.com/schollz/progressbar/v3"
+)
+
+type SolveResult struct {
+	Id              int
+	Puzzle          string
+	Solution        string
+	CorrectSolution string
+	Took            time.Duration
+	Err             error
+}
+
+func Solve(sudokus []*SolveResult, numWorkers int) []*SolveResult {
+	start := time.Now()
+	defer func() {
+		fmt.Printf("Solve(%d) finished in %v\n", numWorkers, time.Since(start).Truncate(time.Millisecond))
+	}()
+
+	var wg sync.WaitGroup
+	jobs := make(chan *SolveResult, len(sudokus))
+	jobResults := make(chan *SolveResult, len(sudokus))
+
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go solveSudokuWorker(jobs, jobResults, &wg)
+	}
+
+	for _, entry := range sudokus {
+		jobs <- entry
+	}
+	close(jobs)
+	go func() {
+		wg.Wait()
+		close(jobResults)
+	}()
+
+	bar := progressbar.Default(int64(len(sudokus)))
+	defer bar.Finish()
+	var result []*SolveResult
+	for jobResult := range jobResults {
+		bar.Add(1)
+		result = append(result, jobResult)
+	}
+	return result
+}
+
+func solveSudokuWorker(jobs <-chan *SolveResult, jobResults chan<- *SolveResult, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for job := range jobs {
+		start := time.Now()
+		board, err := sudoku.Solve(job.Puzzle)
+		if board != nil && err == nil {
+			job.Solution = sudoku.CompactString(board)
+		}
+		job.Err = err
+		job.Took = time.Since(start)
+		jobResults <- job
+	}
+}
